b3d1ae93013814404caad5db194018802d2b0935
VishalS64 <your@email.com>
2025-08-29 23:29:22 +0530

    build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
+// astro.config.mjs
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://tapbill.in',
+  output: 'static',
+  trailingSlash: 'never',
+  prefetch: true,
+  compressHTML: true,
+
+  integrations: [tailwind({
+    applyBaseStyles: true, // optional, includes Tailwind’s base styles
+  })],
+
+  server: {
+    port: 4321,
+    host: true,
+    open: false,
+  },
+});
